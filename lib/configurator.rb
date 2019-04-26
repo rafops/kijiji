597449bb82650dcbb9072d4785e90449c80ca4fc
Rafael Moraes <contact@moraesrafael.com>
2019-04-25 20:06:46 -0400

    accepts config as param

--- lib/configurator.rb
+++ lib/configurator.rb
@@ -4,7 +4,8 @@ class Configurator
   include Singleton
 
   def initialize
-    config_path = File.join(File.dirname(__FILE__), '../config.yml')
+    config_file = ENV['KIJIJI_CONFIG_FILE'] || 'config.yml'
+    config_path = File.join(File.dirname(__FILE__), "../#{config_file}")
     @config = File.exists?(config_path) ? YAML.load(File.read(config_path)) : {}
     @config.each do |k,v|
       @config[k] = ENV[k.upcase] if ENV[k.upcase]
--- run.sh
+++ run.sh
@@ -6,10 +6,14 @@ if [ -z "`docker image ls -q kijiji`" ] ; then
   ./build.sh
 fi
 
-if ! [ -f "./config.yml" ]
+export KIJIJI_CONFIG_FILE="${1:-config.yml}"
+
+if ! [ -f "./${KIJIJI_CONFIG_FILE}" ]
 then
   cp -av ./config.yml.dist ./config.yml
+  unset KIJIJI_CONFIG_FILE
 fi
 
 docker run -it --rm \
+  -e KIJIJI_CONFIG_FILE \
   -v "`pwd`":/root/workdir kijiji
